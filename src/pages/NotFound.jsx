3e1b20a5206422c1d1a29faf179d0625d733c733
Victor Lopes <victor.cabrera0209@gmail.com>
2021-07-18 03:46:21 -0300

    fix stylelint errors

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NotFound() {
   return (
     <main>
-      <h1>Erro 404: Página não encontrada</h1>
+      <h1 className="not-found">Erro 404: Página não encontrada</h1>
     </main>
   );
 }
