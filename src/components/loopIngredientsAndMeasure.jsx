71429adf3c6e510dd08e6525ddb190e996a8bcb2
Victor Lopes <victor.cabrera0209@gmail.com>
2021-07-18 22:20:20 -0300

    fix eslint erros

--- src/components/loopIngredientsAndMeasure.jsx
+++ src/components/loopIngredientsAndMeasure.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ListGroup } from 'react-bootstrap';
 import {
   storageCheckGenerator,
   storageCheckUpdater,
@@ -13,30 +14,38 @@ export default function loopIngredientsAndMeasure(
 ) {
   return (
     mealOrDrinkArray.map((_a, index) => (
-      <section
+    //   <ListGroup key={ `ingredientAndMeasure${index + 1}` }>
+    //   <ListGroup.Item data-testid={ `${index}-ingredient-name-and-measure` }>
+    //     {`${ingredient[`strIngredient${index + 1}`]}
+    //     - ${measure[`strMeasure${index + 1}`]}`}
+    //   </ListGroup.Item>
+    // </ListGroup>
+      <ListGroup
         className={ storageCheckGenerator(id, index) ? 'showCss' : 'hideCss' }
         data-testid={ `${index}-ingredient-step` }
         key={ `ingredientAndMeasure${index + 1}` }
       >
-        <input
-          defaultChecked={ checkBoolean(id, index) }
-          className={ checkBoolean(id, index) ? 'showCss' : 'hideCss' }
-          key={ index }
-          type="checkbox"
-          onClick={ () => {
-            setRefresh(storageCheckUpdater(id, index, refresh));
-          } }
-        />
-        <span className={ checkBoolean(id, index) ? 'showCss' : 'hideCss' }>
-          {' '}
-          {IngredientsAndMeasures.ingredient[`strIngredient${index + 1}`]}
-        </span>
-        <span
-          className={ checkBoolean(id, index) ? 'showCss' : 'hideCss' }
-        >
-          {IngredientsAndMeasures.measure[`strMeasure${index + 1}`]}
-        </span>
-      </section>
+        <ListGroup.Item>
+          <input
+            defaultChecked={ checkBoolean(id, index) }
+            className={ checkBoolean(id, index) ? 'showCss' : 'hideCss' }
+            key={ index }
+            type="checkbox"
+            onClick={ () => {
+              setRefresh(storageCheckUpdater(id, index, refresh));
+            } }
+          />
+          <span className={ checkBoolean(id, index) ? 'showCss' : 'hideCss' }>
+            {' '}
+            {IngredientsAndMeasures.ingredient[`strIngredient${index + 1}`]}
+          </span>
+          <span
+            className={ checkBoolean(id, index) ? 'showCss' : 'hideCss' }
+          >
+            {IngredientsAndMeasures.measure[`strMeasure${index + 1}`]}
+          </span>
+        </ListGroup.Item>
+      </ListGroup>
     ))
   );
 }
